6ffb793adcc06813b05bec147fd62a7dfa00174f
x-gopnik-x <129537995+x-gopnik-x@users.noreply.github.com>
2023-04-07 19:04:36 +0300

    Add files via upload

---
+++ StatsCube.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatsCube : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+         int speed = 10;
+        int Health = 100;
+        int Jump = 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
